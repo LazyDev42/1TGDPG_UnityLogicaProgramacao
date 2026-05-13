8e233a8f764fdc60692734f03d8da6ca1bc60b65
Guilherme M. <guilherme.martins.carvalho07@gmail.com>
2026-05-13 11:59:10 -0300

    Jogo Irado

---
+++ LogicaProgramacao/Assets/Scripts/exemploCollider.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class exemploCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collsion)
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collsion)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collsion)
+    {
+
+    }
+}
